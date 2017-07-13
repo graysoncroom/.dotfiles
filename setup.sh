5dea4768f0bbb8564940f3a4780097d7f4ecb30c
Grayson Croom <grayson.croom@gmail.com>
2017-07-13 14:48:04 -0500

    Made a quick shell script to help with installation. No longer need .scripts directory because of the new + naming convention.

---
+++ setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# I am very new to shell scripting. All comments on what I should improve are welcome!
+
+DIRECTORY_BEFORE_DOTFILE_INSTALL=$(pwd)
+DOTFILE_INSTALL_GITHUB_LINK="https://github.com/graysoncroom/.dotfiles.git"
+
+echo "Before you continue, make sure you backup and remove conflicting dotfiles from your home
+directory."
+echo "You can see the dotfiles that will be installed at $DOTFILE_INSTALL_GITHUB_LINK"
+echo "Enter yes if you want to continue (nothing bad will happen but results will be half assed): "
+
+if [[ $(read) == "yes" ]]; then
+    # Clone repo and go into dotfiles directory
+    cd ~
+    git clone $DOTFILE_INSTALL_GITHUB_LINK
+    cd .dotfiles
+
+    # Start log for ease of debugging
+    echo "Start of script log: $(date)" >> /tmp/dotfile-update.log
+
+    # The files that should be stowed
+    stow_files=$(ls | grep +)
+
+    # Stow those files
+    for file in $stow_files; do
+        stow $file 2> /tmp/dotfile-update.log
+    done
+
+    # A crappy log that may help you get some error infomation
+    echo "Files that should have been stowed:" >> /tmp/dotfile-update.log
+    echo "$stow_files" >> /tmp/dotfile-update.log
+
+    # End log
+    echo "End of script log: $(date)" >> /tmp/dotfile-update.log
+
+    cd DIRECTORY_BEFORE_DOTFILE_INSTALL
+    unset DIRECTORY_BEFORE_DOTFILE_INSTALL
+    unset stow_files
+fi
