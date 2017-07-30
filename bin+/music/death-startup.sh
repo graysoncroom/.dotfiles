2037ca399e01bf60211234dca6d5c74f0165a06d
Grayson Croom <grayson.croom@gmail.com>
2017-07-30 17:56:28 -0500

    Added ranger keybindings. Renamed image extention.

--- bin+/music/death-startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in {21..4000..1}; do
-	beep -f $i -l 2000
-done
-
-for i in {4000..21..1}; do
- 	beep -f $i -l 2000
-done
--- bin+/music/phaser.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-n=3000 
-while [ $n -gt 400 ]; do 
-	beep -f $n -l 5 
-	n=$((n*97/100)) 
-done
